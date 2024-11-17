01009cb5e290d895ae60a2116e0db3014f930340
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-11-17 19:50:23 -0300

    feat:add table with categories for all transactions

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,19 +1,22 @@
 import { ArrowDownUpIcon } from "lucide-react";
 import { Button } from "../_components/ui/button";
 import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
 
 const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
 
   return (
-    <div>
+    <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between p-6">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button>
+        <Button className="rounded-full">
           Adicionar transação
           <ArrowDownUpIcon />
         </Button>
       </div>
+      <DataTable columns={transactionColumns} data={transactions} />
     </div>
   );
 };
