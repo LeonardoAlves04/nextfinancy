c54b928f1f5b25355886f6378b865028fcba9860
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-11-17 19:33:40 -0300

    feat: add transactions page and query with database

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,21 @@
-const TransactionsPage = () => {
-  return <h1>transactions</h1>;
+import { ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div>
+      <div className="flex w-full items-center justify-between p-6">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button>
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default TransactionsPage;
