890fbce847d368ceba9d0aff7ac2427000f15685
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2024-11-21 21:56:06 -0300

    feat: add transaction type badge

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Transaction, TransactionType } from "@prisma/client";
+import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { Badge, CircleIcon } from "lucide-react";
+import TransactionTypeBadge from "../_components/type-badge";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -13,28 +13,7 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
     accessorKey: "type",
     header: "Tipo",
     cell: ({ row: { original: transaction } }) => {
-      if (transaction.type === TransactionType.DEPOSIT) {
-        return (
-          <Badge className="bg-muted font-bold text-primary hover:bg-muted">
-            <CircleIcon className="mr-2 fill-primary" size={10} />
-            Depósito
-          </Badge>
-        );
-      }
-      if (transaction.type === TransactionType.EXPENSE) {
-        return (
-          <Badge className="text-danger bg-danger bg-opacity-10 font-bold">
-            <CircleIcon className="fill-danger mr-2" size={10} />
-            Despesa
-          </Badge>
-        );
-      }
-      return (
-        <Badge className="bg-white bg-opacity-10 font-bold text-white">
-          <CircleIcon className="mr-2 fill-white" size={10} />
-          Investimento
-        </Badge>
-      );
+      <TransactionTypeBadge transaction={transaction} />;
     },
   },
   {
