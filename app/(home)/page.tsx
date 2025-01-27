681b54af80f3c44a25d8ab18f71108debc585c92
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2025-01-27 16:12:54 -0300

    feat: add month filter option on dashboard

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
+import TimeSelect from "./_components/time-select";
 
 const Home = async () => {
   const { userId } = await auth();
@@ -12,7 +13,13 @@ const Home = async () => {
   return (
     <>
       <Navbar />
-      <SummaryCards />
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+        <SummaryCards />
+      </div>
     </>
   );
 };
